071e60821d2a9cd7340c943cd42a4c70e724f186
caio <caiodiasol@proton.me>
2025-06-27 11:16:01 -0300

    adicionando e removendo itens no carrinho

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -11,42 +11,61 @@ import {
 
 import StarWars from '../../assets/images_M35/images/star_wars.png'
 import Tag from '../Tag'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 
-const Cart = () => (
-  <CartContainer>
-    <Overlay />
-    <SideBar>
-      <ul>
-        <CartItem>
-          <img src={StarWars} alt="" />
-          <div>
-            <h3>Nome do jogo</h3>
-            <Tag>RPG</Tag>
-            <Tag>PS5</Tag>
-            <span>R$ 150,00</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-        <CartItem>
-          <img src={StarWars} alt="" />
-          <div>
-            <h3>Nome do jogo</h3>
-            <Tag>RPG</Tag>
-            <Tag>PS5</Tag>
-            <span>R$ 150,00</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-      </ul>
-      <Quantity>2 jogo(s) no carrinho</Quantity>
-      <Prices>
-        Total de R$ 250,00 <span>Em até 6x sem juros</span>
-      </Prices>
-      <Button title="Cique aqui para continuar com a compra" type="button">
-        Continuar com a compra
-      </Button>
-    </SideBar>
-  </CartContainer>
-)
+import { close, remove } from '../../store/reducers/cart'
+import { formataPreco } from '../Products.List'
+import { Item } from '../Gallery/styles'
+
+const Cart = () => {
+  const { isOpen, items } = useSelector((state: RootReducer) => state.cart)
+
+  const dispatch = useDispatch()
+
+  const closeCart = () => {
+    dispatch(close())
+  }
+
+  const getTotalPrice = () => {
+    return items.reduce((acumulador, valorAtual) => {
+      return (acumulador += valorAtual.prices.current!)
+    }, 0)
+  }
+
+  const removeItem = (id: number) => {
+    dispatch(remove(id))
+  }
+
+  return (
+    <CartContainer className={isOpen ? 'is-open' : ''}>
+      <Overlay onClick={closeCart} />
+      <SideBar>
+        <ul>
+          {items.map((item) => (
+            <CartItem key={item.id}>
+              <img src={item.media.thumbnail} alt={item.name} />
+              <div>
+                <h3>{item.name}</h3>
+                <Tag>{item.details.category}</Tag>
+                <Tag>{item.details.system}</Tag>
+                <span>{formataPreco(item.prices.current)}</span>
+              </div>
+              <button onClick={() => removeItem(item.id)} type="button" />
+            </CartItem>
+          ))}
+        </ul>
+        <Quantity>{items.length} jogo(s) no carrinho</Quantity>
+        <Prices>
+          Total de {formataPreco(getTotalPrice())}
+          <span>Em até 6x sem juros</span>
+        </Prices>
+        <Button title="Cique aqui para continuar com a compra" type="button">
+          Continuar com a compra
+        </Button>
+      </SideBar>
+    </CartContainer>
+  )
+}
 
 export default Cart
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,34 +1,47 @@
 import { Link } from 'react-router-dom'
 
-import { HeaderBar, Links, LinkItem, LinkCart } from './styles'
+import { HeaderBar, Links, LinkItem, CartButton } from './styles'
 import logo from '../../assets/images_M35/images/logo.svg'
 import carrinho from '../../assets/images_M35/images/carrinho.svg'
 
-const Header = () => (
-  <HeaderBar>
-    <div>
-      <Link to="/">
-        <img src={logo} alt="Eplay" />
-      </Link>
-      <nav>
-        <Links>
-          <LinkItem>
-            <Link to="/categories">Categorias</Link>
-          </LinkItem>
-          <LinkItem>
-            <a href="#novidades">Novidades</a>
-          </LinkItem>
-          <LinkItem>
-            <a href="#promocoes">Promoções</a>
-          </LinkItem>
-        </Links>
-      </nav>
-    </div>
-    <LinkCart href="#carrinho">
-      0 - produto(s)
-      <img src={carrinho} alt="Carrinho de compras" />
-    </LinkCart>
-  </HeaderBar>
-)
+import { open } from '../../store/reducers/cart'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
+
+const Header = () => {
+  const dispatch = useDispatch()
+  const { items } = useSelector((state: RootReducer) => state.cart)
+
+  const openCart = () => {
+    dispatch(open())
+  }
+
+  return (
+    <HeaderBar>
+      <div>
+        <Link to="/">
+          <img src={logo} alt="Eplay" />
+        </Link>
+        <nav>
+          <Links>
+            <LinkItem>
+              <Link to="/categories">Categorias</Link>
+            </LinkItem>
+            <LinkItem>
+              <a href="#novidades">Novidades</a>
+            </LinkItem>
+            <LinkItem>
+              <a href="#promocoes">Promoções</a>
+            </LinkItem>
+          </Links>
+        </nav>
+      </div>
+      <CartButton onClick={openCart}>
+        {items.length} - produto(s)
+        <img src={carrinho} alt="Carrinho de compras" />
+      </CartButton>
+    </HeaderBar>
+  )
+}
 
 export default Header
